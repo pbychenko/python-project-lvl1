5b855664e5382aea269baae7ca6897c4d92fe69b
Pavel Bychenko <pavelv87@gmail.com>
2022-11-29 21:03:46 +0600

    finish step 3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-# import prompt
+
+from ..cli import welcome_user
+
 
 def main():
-    print("Welcome to the Brain Games!!")
-		print('test')
-		# print("my name is Jack")
+	print("Welcome to the Brain Games!?")
+	welcome_user()
 
 if __name__ == '__main__':
     main()
