000f91ee908b5e595853d58368c783c5692dac5d
Pavel Bychenko <pavelv87@gmail.com>
2022-11-29 18:39:23 +0600

    fix changes

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+# import prompt
 
 def main():
     print("Welcome to the Brain Games!!")
-
+		print('test')
+		# print("my name is Jack")
 
 if __name__ == '__main__':
     main()
